de65bcc083ef700456726b1fc20ec75c41aacec7
David Kwon <david.kwon@clover.com>
2024-10-29 13:26:43 -0600

    Implement SoftwareEngineer class and modify error in associated tests

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,8 +1,21 @@
-class SoftwareEngineer {
+const { Employee } = require("./Employee");
 
+class SoftwareEngineer extends Employee {
+    #salary;
+    #isHired;
+    #programmingLanguages;
+    constructor(name, position, salary, programmingLanguages) {
+        super(name, position, salary, programmingLanguages);
+        this.#programmingLanguages = programmingLanguages;
+    }
 
+    getProgrammingLanguages() {
+        return this.#programmingLanguages;
+    }
 
-    
+    setProgrammingLanguage(language) {
+        this.#programmingLanguages.push(language);
+    }
 }
 
 module.exports = {
