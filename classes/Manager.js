20bb8c4bb2ae757b037084889896d7f5e98f9b3c
David Kwon <david.kwon@clover.com>
2024-10-29 13:17:47 -0600

    Implement Manager class and fix error in associated tests

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,8 +1,22 @@
-class Manager {
+const { Employee } = require("./Employee");
 
+class Manager extends Employee {
+    #salary;
+    #isHired;
+    #employeesManaged;
+    constructor(name, position, salary, department) {
+        super(name, position, salary);
+        this.department = department;
+        this.#employeesManaged = [];
+    }
 
+    getEmployeesManaged() {
+        return this.#employeesManaged;
+    }
 
-    
+    addEmployeeManaged(employee) {
+        this.#employeesManaged.push(employee);
+    }
 }
 
 module.exports = {
